ba28a6334d96fed0d55d9c63981395132ab7b43f
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-04-30 19:45:08 +0330

    refactor & shoContact feature

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import readLine from "readline/promises";
 import { stdin as input, stdout as output } from "process";
-import { join } from "path";
 
 const rl = readLine.createInterface({ input, output });
 
@@ -8,22 +7,45 @@ const contactsList = [];
 
 console.log("--- ContactList ---");
 
-const firstName = await rl.question("First Name: ");
-const lastName = await rl.question("Last Name: ");
+async function addNewContact() {
+  const firstName = await rl.question("First Name: ");
+  const lastName = await rl.question("Last Name: ");
 
-const newContact = {
-  id: contactsList.length,
-  firstName,
-  lastName,
-};
+  const newContact = {
+    id: contactsList.length,
+    firstName,
+    lastName,
+  };
 
-contactsList.push(newContact);
+  contactsList.push(newContact);
+}
 
-console.log("Contacts List:");
+function showContactsList() {
+  const formatedContactsList = contactsList
+    .map(({ id, firstName, lastName }) => `#${id} ${firstName} ${lastName}`)
+    .join("\n");
 
-const formatedContactsList = contactsList
-  .map(({ id, firstName, lastName }) => `#${id} ${firstName} ${lastName}`)
-  .join("\n");
-console.log(formatedContactsList);
+  console.log("Contacts List:");
+  console.log(formatedContactsList);
+}
 
-rl.close();
+function quit() {
+  rl.close();
+}
+
+async function help() {
+  console.log("n: Add new contact\nl: show contacts list\nq: quit");
+  const action = await rl.question("Enter your action:");
+
+  if (action === "n") {
+    await addNewContact();
+  } else if (action === "l") {
+    showContactsList();
+  } else {
+    quit();
+    return;
+  }
+  help();
+}
+
+help();
