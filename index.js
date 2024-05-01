e11525713f0b3a5b03f42815825b48ff66ed1b07
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-05-01 11:29:11 +0330

    delete contact

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import readLine from "readline/promises";
 import { stdin as input, stdout as output } from "process";
+import fs from "fs/promises";
+
+const CONTACTS_LIST_FILE_PATH = "./data/contacts-list.json";
 
 const rl = readLine.createInterface({ input, output });
 
@@ -7,17 +10,63 @@ const contactsList = [];
 
 console.log("--- ContactList ---");
 
+async function loadContacts() {
+  try {
+    const contactsListJSON = await fs.readFile(
+      CONTACTS_LIST_FILE_PATH,
+      "utf-8"
+    );
+    contactsList.push(...JSON.parse(contactsListJSON));
+  } catch (error) {
+    throw error;
+  }
+}
+async function saveContacts() {
+  try {
+    const contactsListJSON = JSON.stringify(contactsList);
+    await fs.writeFile(CONTACTS_LIST_FILE_PATH, contactsListJSON);
+  } catch (error) {
+    throw error;
+  }
+}
+
 async function addNewContact() {
   const firstName = await rl.question("First Name: ");
   const lastName = await rl.question("Last Name: ");
 
+  const lastContact = contactsList[contactsList.length - 1];
+  const id = lastContact ? lastContact.id + 1 : 1;
+
   const newContact = {
-    id: contactsList.length,
+    id,
     firstName,
     lastName,
   };
 
   contactsList.push(newContact);
+  saveContacts();
+}
+
+async function deleteContact() {
+  if (contactsList.length < 1) {
+    console.error("There is no contact on the list");
+    return;
+  }
+
+  showContactsList();
+
+  const contactID = await rl.question("Delete ID: ");
+  const contactIndex = contactsList.findIndex(
+    ({ id }) => id === Number(contactID)
+  );
+
+  if (contactIndex < 0) {
+    console.error("Invalid ID");
+    return;
+  }
+
+  contactsList.splice(contactIndex, 1);
+  saveContacts();
 }
 
 function showContactsList() {
@@ -34,18 +83,30 @@ function quit() {
 }
 
 async function help() {
-  console.log("n: Add new contact\nl: show contacts list\nq: quit");
+  console.log(
+    "n: add new contact\nd: delete acontact\nl: show contacts list\nq: quit"
+  );
+
+  console.log("----------");
   const action = await rl.question("Enter your action:");
 
   if (action === "n") {
     await addNewContact();
+  } else if (action === "d") {
+    await deleteContact();
   } else if (action === "l") {
     showContactsList();
   } else {
     quit();
     return;
   }
+  console.log("----------");
   help();
 }
 
-help();
+async function main() {
+  await loadContacts();
+  help();
+}
+
+await main();
