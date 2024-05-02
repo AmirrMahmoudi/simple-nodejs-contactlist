88dd1db2b546d8e49e888165b4814c78eeacddf1
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-05-02 14:24:21 +0330

    delete contact with express server

--- server/routes.js
+++ server/routes.js
@@ -38,6 +38,31 @@ router.post("/new", (req, res) => {
   res.send(`The contact "#${id} ${firstName} ${lastName}" has been created!`);
 });
 
+router.delete(`/:id`, (req, res) => {
+  if (contactsList.length < 1) {
+    res.status(400).send({
+      message: "There is no contact on the list",
+    });
+    return;
+  }
+
+  const contactIndex = contactsList.findIndex(
+    ({ id }) => id === Number(req.params.id)
+  );
+
+  if (contactIndex < 0) {
+    res.status(400).send({
+      message: "Invalid ID",
+    });
+    return;
+  }
+
+  contactsList.splice(contactIndex, 1);
+  saveContacts(contactsList);
+
+  res.send(`Contact #${req.params.id} has been created!`);
+});
+
 const loadedContacts = await loadContacts();
 contactsList.push(...loadedContacts);
 
